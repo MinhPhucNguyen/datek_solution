3e72b274692fb7d97be2c0099c452b700847692a
phuc <phunm@tigren.com>
2024-10-03 21:02:22 +0700

    setup router

--- datek_slt_frontend/src/router/app.js
+++ datek_slt_frontend/src/router/app.js
@@ -1,3 +1,15 @@
-const app = [];
+const app = [
+  {
+    path: "/",
+    component: () => import("@/layouts/app.vue"),
+    children: [
+      {
+        path: "/",
+        component: () => import("@/pages/HomePage.vue"),
+        name: "home",
+      },
+    ],
+  },
+];
 
 export default app;
