df711ce9773ec838adea2e752d464c92fdc6b608
phuc <phunm@tigren.com>
2024-10-28 22:14:46 +0700

    Create Categories table

---
+++ datek_slt_backend/app/Models/Category.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'category_name',
+        'description'
+    ];
+}
