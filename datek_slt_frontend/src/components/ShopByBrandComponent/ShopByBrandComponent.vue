a79d82e1cc496c89c7e1e55e7f970f306aba58f0
phuc <phunm@tigren.com>
2024-11-16 10:05:02 +0700

    Create Shop by brand component

---
+++ datek_slt_frontend/src/components/ShopByBrandComponent/ShopByBrandComponent.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="shop-by-brand-block">
+    <div class="container">
+      <div class="shop-by-brand-title">
+        <p>Shop by Brand</p>
+      </div>
+      <div class="shop-by-brand-content">
+        <div class="brand-logos">
+          <div class="brand-logo">
+            <img
+              src="../../assets/images/brand-logos/lenovo.png"
+              alt="brand-logo"
+            />
+          </div>
+          <div class="brand-logo">
+            <a href="#">
+              <img
+                src="../../assets/images/brand-logos/dell.png"
+                alt="brand-logo"
+              />
+            </a>
+          </div>
+          <div class="brand-logo">
+            <a href="#">
+              <img
+                src="../../assets/images/brand-logos/getac.svg"
+                alt="brand-logo"
+              />
+            </a>
+          </div>
+          <div class="brand-logo">
+            <a href="#">
+              <img
+                src="../../assets/images/brand-logos/microsoft.svg"
+                alt="brand-logo"
+              />
+            </a>
+          </div>
+          <div class="brand-logo">
+            <a href="#">
+              <img
+                src="../../assets/images/brand-logos/apple.png"
+                alt="brand-logo"
+              />
+            </a>
+          </div>
+          <div class="brand-logo">
+            <a href="#">
+              <img
+                src="../../assets/images/brand-logos//dell.png"
+                alt="brand-logo"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+@import url(ShopByBrandComponent.scss);
+</style>
--- datek_slt_frontend/src/pages/HomePage.vue
+++ datek_slt_frontend/src/pages/HomePage.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <BannerComponent />
+    <ShopByBrandComponent />
   </div>
 </template>
 
 <script setup>
 import BannerComponent from '@/components/BannerComponent/BannerComponent.vue';
+import ShopByBrandComponent from '@/components/ShopByBrandComponent/ShopByBrandComponent.vue';
 </script>
 
 <style scoped lang="scss"></style>
