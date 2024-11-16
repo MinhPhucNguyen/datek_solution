6499dd828a8adb5902410a88925513f3b777d75f
phuc <phunm@tigren.com>
2024-11-16 12:55:09 +0700

    Edit footer on home page

--- datek_slt_frontend/src/pages/HomePage.vue
+++ datek_slt_frontend/src/pages/HomePage.vue
@@ -3,7 +3,6 @@
     <BannerComponent />
     <ShopByBrandComponent />
     <LatestProductListComponent />
-    <FooterComponent />
   </div>
 </template>
 
@@ -11,7 +10,6 @@
 import BannerComponent from '@/components/BannerComponent/BannerComponent.vue';
 import ShopByBrandComponent from '@/components/ShopByBrandComponent/ShopByBrandComponent.vue';
 import LatestProductListComponent from '@/components/LatestProductListComponent/LatestProductListComponent.vue';
-import FooterComponent from '@/components/FooterComponent/FooterComponent.vue';
 </script>
 
 <style scoped lang="scss"></style>
