24399d3d0f08a50b5985fb12c511872cf7e6fe85
phuc <phunm@tigren.com>
2024-11-16 12:43:20 +0700

    Update app layout

--- datek_slt_frontend/src/layouts/app.vue
+++ datek_slt_frontend/src/layouts/app.vue
@@ -2,11 +2,13 @@
   <div>
     <HeaderComponent />
     <router-view></router-view>
+    <FooterComponent />
   </div>
 </template>
 
 <script setup>
 import HeaderComponent from "@/components/HeaderComponent/HeaderComponent.vue";
+import FooterComponent from "@/components/FooterComponent/FooterComponent.vue";
 </script>
 
 <style scoped></style>
