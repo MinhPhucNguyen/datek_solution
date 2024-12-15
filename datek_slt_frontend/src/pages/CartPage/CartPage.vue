01d0a9ee551aa6a25bbdce84e5e6f2924d34c6a6
phuc <phunm@tigren.com>
2024-12-15 10:01:28 +0700

    Update Cart Page

--- datek_slt_frontend/src/pages/CartPage/CartPage.vue
+++ datek_slt_frontend/src/pages/CartPage/CartPage.vue
@@ -5,57 +5,59 @@
       <div v-if="cartItems.length > 0">
         <table class="cart-table">
           <thead>
-          <tr>
-            <th></th>
-            <th>Sản phẩm</th>
-            <th>Đơn giá</th>
-            <th>Số lượng</th>
-            <th>Số tiền</th>
-            <th></th>
-          </tr>
+            <tr>
+              <th></th>
+              <th>Sản phẩm</th>
+              <th>Đơn giá</th>
+              <th>Số lượng</th>
+              <th>Số tiền</th>
+              <th></th>
+            </tr>
           </thead>
           <tbody>
-          <tr v-for="(item, index) in cartItems" :key="item.product_id">
-            <td><input type="checkbox"/></td>
-            <td>
-              <div class="cart-item-details">
-                <img
+            <tr v-for="(item, index) in cartItems" :key="item.product_id">
+              <td><input type="checkbox" /></td>
+              <td>
+                <div class="cart-item-details">
+                  <img
                     :src="item.product.product_images[0].image_url"
                     alt="Product Image"
-                />
-                <div class="product-name">
-                  <p>{{ item.product.name }}</p>
+                  />
+                  <div class="product-name">
+                    <p>{{ item.product.name }}</p>
+                  </div>
                 </div>
-              </div>
-            </td>
-            <td>{{ formatCurrency(item.product.price) }}</td>
-            <td>
-              <div class="quantity-control">
-                <button
+              </td>
+              <td>{{ formatCurrency(item.product.price) }}</td>
+              <td>
+                <div class="quantity-control">
+                  <button
                     class="quantity-btn decrease-btn"
                     @click="updateQuantity(index, item.quantity - 1)"
-                >
-                  <font-awesome-icon :icon="['fas', 'minus']"/>
-                </button>
-                <input
+                  >
+                    <font-awesome-icon :icon="['fas', 'minus']" />
+                  </button>
+                  <input
                     type="number"
                     v-model="item.quantity"
                     class="quantity-input"
                     readonly
-                />
-                <button
+                  />
+                  <button
                     class="quantity-btn increase-btn"
                     @click="updateQuantity(index, item.quantity + 1)"
-                >
-                  <font-awesome-icon :icon="['fas', 'plus']"/>
+                  >
+                    <font-awesome-icon :icon="['fas', 'plus']" />
+                  </button>
+                </div>
+              </td>
+              <td>{{ formatCurrency(item.product.price * item.quantity) }}</td>
+              <td>
+                <button class="remove-btn" @click="removeItem(index)">
+                  Xóa
                 </button>
-              </div>
-            </td>
-            <td>{{ formatCurrency(item.product.price * item.quantity) }}</td>
-            <td>
-              <button class="remove-btn" @click="removeItem(index)">Xóa</button>
-            </td>
-          </tr>
+              </td>
+            </tr>
           </tbody>
         </table>
 
@@ -68,15 +70,14 @@
       <div v-else>
         <p>Không có sản phẩm nào trong giỏ hàng của bạn.</p>
       </div>
-
     </div>
   </div>
 </template>
 
 <script setup>
-import {computed, onBeforeMount, ref} from "vue";
-import {useStore} from "vuex";
-import {formatCurrency} from "@/utils/formatCurrency";
+import { computed, onBeforeMount, ref } from "vue";
+import { useStore } from "vuex";
+import { formatCurrency } from "@/utils/formatCurrency";
 
 const store = useStore();
 const cartItems = ref([]);
@@ -88,11 +89,10 @@ onBeforeMount(async () => {
 
 const totalPrice = computed(() => {
   return cartItems.value.reduce(
-      (total, item) => total + item.product.price * item.quantity,
-      0
+    (total, item) => total + item.product.price * item.quantity,
+    0
   );
 });
-
 
 const updateQuantity = async (index, newQuantity) => {
   newQuantity = Math.max(newQuantity, 1);
@@ -107,7 +107,6 @@ const updateQuantity = async (index, newQuantity) => {
     const updatedCart = store.getters["cart/getCartItems"];
     updatedCart[index].quantity = newQuantity;
     cartItems.value = updatedCart;
-    
   } catch (error) {
     console.error(error);
   }
