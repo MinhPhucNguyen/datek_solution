1d61592c306c3a96d58dbaaf2addb1d8623c55c1
phuc <phunm@tigren.com>
2024-12-18 01:38:16 +0700

    Update style and create slider for latest product

--- datek_slt_frontend/src/components/LatestProductListComponent/LatestProductListComponent.vue
+++ datek_slt_frontend/src/components/LatestProductListComponent/LatestProductListComponent.vue
@@ -4,19 +4,14 @@
       <div class="latest-produtct-list-title">
         <p>Sản phẩm mới nhất</p>
       </div>
-      <div class="latest-product-list-content">
+      <div class="latest-product-list-content" v-if="latestProducts.length > 0">
         <div class="product-slider">
-          <button class="slider-btn left" @click="slideLeft">
+          <button class="slider-btn left" @click="prevSlide" :disabled="isPrevDisabled">
             <i class="fa-solid fa-chevron-left"></i>
           </button>
 
           <div class="product-list-wrapper">
-            <div
-              class="product-list-latest"
-              :style="{
-                transform: `translateX(calc(-100% * ${currentIndex}))`,
-              }"
-            >
+            <div class="product-list-latest" :style="sliderStyle">
               <ProductItemComponent
                 v-for="product in latestProducts"
                 :key="product.id"
@@ -25,7 +20,7 @@
             </div>
           </div>
 
-          <button class="slider-btn right" @click="slideRight">
+          <button class="slider-btn right" @click="nextSlide"  :disabled="isNextDisabled">
             <i class="fa-solid fa-chevron-right"></i>
           </button>
         </div>
@@ -36,10 +31,12 @@
 
 <script setup>
 import axios from "axios";
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import ProductItemComponent from "../ProductItemComponent/ProductItemComponent.vue";
 
 const latestProducts = ref([]);
+const currentIndex = ref(0);
+const productsPerPage = 5;
 
 const fetchProducts = async () => {
   try {
@@ -53,6 +50,25 @@ const fetchProducts = async () => {
 onMounted(() => {
   fetchProducts();
 });
+
+const nextSlide = () => {
+  if (currentIndex.value < latestProducts.value.length - productsPerPage) {
+    currentIndex.value += productsPerPage;
+  }
+};
+
+const prevSlide = () => {
+  if (currentIndex.value > 0) {
+    currentIndex.value -= productsPerPage;
+  }
+};
+
+const sliderStyle = computed(() => ({
+  transform: `translateX(-${(currentIndex.value / productsPerPage) * 100}%)`,
+}));
+
+const isPrevDisabled = computed(() => currentIndex.value === 0);
+const isNextDisabled = computed(() => currentIndex.value >= latestProducts.value.length - productsPerPage);
 </script>
 
 <style scoped>
