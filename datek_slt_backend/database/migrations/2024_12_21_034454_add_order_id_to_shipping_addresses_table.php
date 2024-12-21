c579bb29f8441c6e5e2d2e9d5ec8aff07d94f811
phuc <phunm@tigren.com>
2024-12-21 10:47:19 +0700

    Add new column to shipping addresses table

---
+++ datek_slt_backend/database/migrations/2024_12_21_034454_add_order_id_to_shipping_addresses_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('shipping_addresses', function (Blueprint $table) {
+            $table->unsignedBigInteger('order_id')->nullable()->after('id');
+            $table->foreign('order_id')->references('order_id')->on('orders')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('shipping_addresses', function (Blueprint $table) {
+            $table->dropForeign(['order_id']);
+            $table->dropColumn('order_id');
+        });
+    }
+};
